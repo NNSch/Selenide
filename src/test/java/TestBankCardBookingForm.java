877076ecb713e915db7ba176d3a528979eee3989
Natalia Shevchenko <ledidile@rambler.ru>
2023-09-07 21:50:44 +0300

    commit test

--- src/test/java/TestBankCardBookingForm.java
+++ src/test/java/TestBankCardBookingForm.java
@@ -19,15 +19,15 @@ public class TestBankCardBookingForm {
 
         open("http://localhost:9999");
         $("[data-test-id='city'] input").setValue("Казань");
-        String currentDate = generateDate(4, "dd.MM.yyyy");
+        String planningDate = generateDate(4, "dd.MM.yyyy");
         $("[data-test-id='date'] input").sendKeys(Keys.chord(Keys.SHIFT, Keys.HOME), Keys.DELETE);
-        $("[data-test-id='date'] input").setValue(currentDate);
+        $("[data-test-id='date'] input").setValue(planningDate);
         $("[data-test-id='name'] input").setValue("Васильев Василий");
         $("[data-test-id='phone'] input").setValue("+79123456789");
         $("[data-test-id='agreement']").click();
         $("button.button").click();
         $("notification__content")
                 .shouldBe(Condition.visible, Duration.ofSeconds(15))
-                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + currentDate));
+                .shouldHave(Condition.exactText("Встреча успешно забронирована на " + planningDate));
     }
 }
